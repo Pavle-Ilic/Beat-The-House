437ac2e94a8160952e355e697b419f50645bd105
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-01-07 15:50:56 -0500

    implemented backwards propagation

--- DenseLayerClass.py
+++ DenseLayerClass.py
@@ -25,4 +25,19 @@ class Dense():
 
 
     def backward(self, output_gradient, learning_rate):
-        pass
+
+        # Calculate the derivative of the error wrt the weights
+        # dE/dW = dE/dY dot X^t (X transposed)
+        self.weights_gradient = np.dot(output_gradient,self.input.T)
+
+        # update the weights and bias based off the weights gradient
+        self.weights -= learning_rate * self.weights_gradient
+        self.bias -= learning_rate * output_gradient   # note that dE/dB = dE/dY
+
+        # return the derivative of the error wrt the input
+        # dE/dX = W^t dot dE/dY
+        self.input_gradient = np.dot(self.weights.T, output_gradient)
+        return self.input_gradient
+
+
+
